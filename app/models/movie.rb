59a2e766f19ea80cb9fbfbe8ae2680624ba25919
chiu <chiucode@gmail.com>
2015-05-26 22:12:11 +0000

    great

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,29 @@
 class Movie < ActiveRecord::Base
+  validates :title,
+  presence: true
+
+  validates :director,
+  presence: true
+
+  validates :runtime_in_minutes,
+  numericality: { only_integer: true }
+
+  validates :description,
+  presence: true
+
+  validates :poster_image_url,
+  presence: true
+
+  validates :release_date,
+  presence: true
+
+  validate :release_date_is_in_the_future
+
+  protected
+
+  def release_date_is_in_the_future
+    if release_date.present?
+       errors.add(:release_date, "should probably be in future.") if release_date < Date.today
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526200452) do
+ActiveRecord::Schema.define(version: 20150526220548) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20150526200452) do
     t.text     "description"
     t.string   "poster_image_url"
     t.datetime "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
